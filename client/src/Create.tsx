72be20e59f0b6f9a7dd3a8f6fc2400c7300d9d55
Kevin Liu <kevinliu04@gmail.com>
2024-02-04 12:34:08 -0800

    create a room

--- client/src/Create.tsx
+++ client/src/Create.tsx
@@ -1,1 +1,57 @@
-export default function Create() {}
+import { SyntheticEvent, useState } from "react";
+
+export default function Create() {
+    const [checked, setChecked] = useState(false);
+    const [key, setKey] = useState("");
+
+    function createJoinCode() {
+        // https://stackoverflow.com/a/44622300
+        setKey(
+            Array.from(Array(20), () =>
+                Math.floor(Math.random() * 36).toString(36)
+            ).join("")
+        );
+    }
+
+    function handleSubmit(event: SyntheticEvent<HTMLFormElement, SubmitEvent>) {
+        event.preventDefault();
+    }
+
+    function handleCheck(event: {
+        currentTarget: { checked: boolean | ((prevState: boolean) => boolean) };
+    }) {
+        setChecked(event.currentTarget.checked);
+
+        if (checked) {
+            createJoinCode();
+        } else {
+            setKey("");
+        }
+    }
+
+    return (
+        <div>
+            <h1>Create a Room:</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="room-name">Room name:</label>
+                <input type="text" name="room-name" id="room-name" />
+                <label htmlFor="private">Private?</label>
+                <input
+                    type="checkbox"
+                    name="private"
+                    id="private"
+                    onChange={handleCheck}
+                />
+                <div>{checked && <p>Your private key is: {key}</p>}</div>
+                <input type="submit" value="Create!" />
+                <button
+                    onClick={() => {
+                        navigator.clipboard.writeText(key);
+                    }}
+                >
+                    Copy
+                </button>
+            </form>
+        </div>
+    );
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import Menu from "./Menu";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Lobby from "./Lobby";
+import Create from "./Create";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
@@ -14,6 +15,7 @@ root.render(
                 <Route path="/" element={<Menu />} />
                 {/* <App /> */}
                 <Route path="/lobby" element={<Lobby />}></Route>
+                <Route path="/create" element={<Create />}></Route>
             </Routes>
         </BrowserRouter>
     </React.StrictMode>
