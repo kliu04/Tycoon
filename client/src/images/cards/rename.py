90b83789c643072eafff935ec0f549687aa7323c
Kevin Liu <kevinliu04@gmail.com>
2024-02-11 17:09:00 -0800

    renamed cards

--- client/src/images/cards/rename.py
+++ client/src/images/cards/rename.py
@@ -1,3 +1,7 @@
+'''
+Rename downloaded card names from e.g., "9C.svg" to "9_of_Clubs.svg"
+'''
+
 import os
 
 trans = {
@@ -7,10 +11,29 @@ trans = {
     "D" : "Diamonds",
 }
 
+trans_face = {
+    "A" : "Ace",
+    "T" : "10",
+    "J" : "Jack",
+    "Q" : "Queen",
+    "K" : "King",
+}
+
 def main():
     for i in range(2, 10):
         for j in ["S", "H", "C", "D"]:
-            os.rename(f"{i}{j}.svg", f"{i}_of_{trans[j]}.svg")
+            try:
+                os.rename(f"{i}{j}.svg", f"{i}_of_{trans[j]}.svg")
+            except:
+                continue
+
+    for i in ["A", "T", "J", "Q", "K"]:
+        for j in ["S", "H", "C", "D"]:
+            try:
+                os.rename(f"{i}{j}.svg", f"{trans_face[i]}_of_{trans[j]}.svg")
+            except:
+                continue        
+
 
 if __name__ == "__main__":
     main()
